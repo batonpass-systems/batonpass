3e45a03e52c89cb8ddc693611e939ef795937c2b
Brad Clawsie <brad@batonpass.systems>
2026-07-14 16:46:47 -0500

    use sqlx ! syntax to force NOT NULL

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ mod batonpass;
 use batonpass::state::State;
 use tracing::info;
 
+#[allow(dead_code)]
+struct Ping {
+    one: i32,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), batonpass::state::Error> {
     dotenvy::dotenv().ok();
@@ -10,7 +15,7 @@ async fn main() -> Result<(), batonpass::state::Error> {
         .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
         .init();
     let state = State::unit().await?;
-    let _row: (i32,) = sqlx::query_as("select 1")
+    let _row = sqlx::query_as!(Ping, r#"select 1 as "one!""#)
         .fetch_one(state.random_replica())
         .await
         .expect("select 1");
